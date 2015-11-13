55c8f00bc7ac99b6996e9315ccbf8b9001a7953b
Vein Kong <gamblingfool28@yahoo.com>
2015-11-12 19:15:37 -0800

    added end line

--- linearRegression.R
+++ linearRegression.R
@@ -1,4 +1,4 @@
-library("glmnet")  # For ridge regression fitting. It also supports elastic-net and LASSO models 
+library("glmnet")  # For ridge regression fitting. It also supports elastic-net and LASSO models
 library("gbm")  # For Gradient-Boosting
 library("rpart")  # For building decision trees
 library("CORElearn")
@@ -35,22 +35,22 @@ area_test_error <- mean((test_set$jpt - test_set$tjpt)^2)
 
 ## comparison plot of lm vs. area-based
 lm_offset <- lm_predictions - test_set$tjpt
-h<-hist(lm_offset, breaks=50, col="red", xlab="Estimated Jpt - True Jpt", main="Linear regression vs. Area-based") 
-xfit<-seq(min(lm_offset),max(lm_offset),length=40) 
-yfit<-dnorm(xfit,mean=mean(lm_offset),sd=sd(lm_offset)) 
-yfit <- yfit*diff(h$mids[1:2])*length(lm_offset) 
+h<-hist(lm_offset, breaks=50, col="red", xlab="Estimated Jpt - True Jpt", main="Linear regression vs. Area-based")
+xfit<-seq(min(lm_offset),max(lm_offset),length=40)
+yfit<-dnorm(xfit,mean=mean(lm_offset),sd=sd(lm_offset))
+yfit <- yfit*diff(h$mids[1:2])*length(lm_offset)
 lines(xfit, yfit, col="black", lwd=2)
 
 test_offset <- test_set$jpt - test_set$tjpt
 #x2 <- testdf$offset
-#h<-hist(x2, breaks=20, col="blue", xlab="h(x) - y on the test set", 
-#main="Linear regression") 
-xfit2<-seq(min(test_offset),max(test_offset),length=40) 
-yfit2<-dnorm(xfit2,mean=mean(test_offset),sd=sd(test_offset)) 
-yfit2 <- yfit2*diff(h$mids[1:2])*length(test_offset) 
+#h<-hist(x2, breaks=20, col="blue", xlab="h(x) - y on the test set",
+#main="Linear regression")
+xfit2<-seq(min(test_offset),max(test_offset),length=40)
+yfit2<-dnorm(xfit2,mean=mean(test_offset),sd=sd(test_offset))
+yfit2 <- yfit2*diff(h$mids[1:2])*length(test_offset)
 lines(xfit2, yfit2, col="green", lwd=2)
 
-  
+
 ## plot the t
 
 #for(i in 1 :8 ){
@@ -61,12 +61,12 @@ coef(OLS_model)
 response_column <- which(colnames(traindf) == "tjpt")
 #trainerror[i] <-
 mean(summary(OLS_model)$residuals^2)
-#areatrainerr[i] <- 
+#areatrainerr[i] <-
 mean((traindf$tjpt - traindf$jpt)^2)
 predictions_ols <- predict(OLS_model, testdf[, -response_column])
-#testerror[i] <- 
+#testerror[i] <-
 mean((predictions_ols - testdf$tjpt)^2)
-#areatesterr[i] <- 
+#areatesterr[i] <-
 mean((testdf$tjpt - testdf$jpt)^2)
 
 #}
